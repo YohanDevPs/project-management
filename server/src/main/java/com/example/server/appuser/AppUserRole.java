937f8afdeb147fccbf1220d61d121cf5e3bfa550
Yohan Silva <yohanps.dev@gmail.com>
2023-01-24 21:09:31 -0300

    feat: create User structure

---
+++ server/src/main/java/com/example/server/appuser/AppUserRole.java
@@ -0,0 +1,6 @@
+package com.example.server.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
---
+++ server/src/main/java/com/example/server/registration/RegistrationController.java
@@ -0,0 +1,14 @@
+package com.example.server.registration;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1/registration")
+@AllArgsConstructor
+public class RegistrationController {
+
+
+
+}

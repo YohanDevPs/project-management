2d2c534551a1df28aff9874a3dfa85f3efaff15f
Yohan Silva <yohanps.dev@gmail.com>
2023-01-25 16:13:58 -0300

    feat: implement confirm password

--- client/src/app/security/sign-up/sign-up.component.ts
+++ client/src/app/security/sign-up/sign-up.component.ts
@@ -1,5 +1,27 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ValidatorFn,
+  ValidationErrors,
+  AbstractControl,
+} from '@angular/forms';
+
+export function passwordsMatchValidator(): ValidatorFn {
+  return (control: AbstractControl): ValidationErrors | null => {
+    const password = control.get('password')?.value;
+    const confirmPassword = control.get('confirmPassword')?.value;
+
+    if (password && confirmPassword && password !== confirmPassword) {
+      return {
+        passwordsDontMatch: true,
+      };
+    }
+
+    return null;
+  };
+}
 
 @Component({
   selector: 'app-sign-up',
@@ -7,24 +29,31 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
   styleUrls: ['./sign-up.component.scss'],
 })
 export class SignUpComponent implements OnInit {
-  signUpForm = new FormGroup({
-    name: new FormControl('', [Validators.required]),
-    email: new FormControl('', [Validators.required]),
-    password: new FormControl('', [Validators.required]),
-    confirmPassword: new FormControl('', [Validators.required])
-  });
+  signUpForm = new FormGroup(
+    {
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', [Validators.required]),
+      confirmPassword: new FormControl('', [Validators.required]),
+    },
+    { validators: passwordsMatchValidator() }
+  );
 
   ngOnInit(): void {}
 
-  get userName() {
+  get name() {
     return this.signUpForm.get('name');
   }
 
-  get userPassord() {
+  get password() {
     return this.signUpForm.get('password');
   }
 
-  get userEmail() {
+  get email() {
     return this.signUpForm.get('password');
+  }
+
+  get confirmPassword() {
+    return this.signUpForm.get('confirmPassword');
   }
 }

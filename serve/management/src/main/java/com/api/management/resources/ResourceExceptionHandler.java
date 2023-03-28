e0046f3656d90a93dd428724ee27e8dae651c5eb
Yohan Silva <yohanps.dev@gmail.com>
2023-03-28 18:44:38 -0300

    feat: create resources

---
+++ serve/management/src/main/java/com/api/management/resources/ResourceExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.api.management.resources;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<StandartError> entityNotFound(EntityNotFoundException exception, HttpServletRequest request){
+        StandartError err = new StandartError();
+        err.setTimestamp(Instant.now());
+        err.setStatus(HttpStatus.NOT_FOUND.value());
+        err.setError("Resource not found");
+        err.setMessage(exception.getMessage());
+        err.setPath(request.getRequestURI());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+    }
+}
